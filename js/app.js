4ef8dba41cbee314702c95ee8349bcc9876816ad
chapin11 <seedyou36@gmail.com>
2025-01-21 18:39:24 +0300

    add added a boss, an escape exit button), etc.

---
+++ js/app.js
@@ -0,0 +1,678 @@
+let arrayword = [
+  "javascript",
+  "good",
+  "application",
+  "hello",
+  "programmist",
+  "newyear",
+  "chef",
+  "alps",
+  "dare",
+  "servo",
+  "sob",
+  "grass",
+  "hater",
+  "cloth",
+  "roads",
+  "herd",
+  "wrongfully",
+  "writings",
+  "mink",
+  "minimum",
+  "gardens",
+  "gap",
+  "funniest",
+  "fundraising",
+  "frightfully",
+  "freakish",
+  "frazzled",
+  "abilities",
+  "abruptly",
+  "actor",
+  "acrobat",
+  "actions",
+  "administrator",
+  "adrenaline",
+  "advertise",
+  "alchemy",
+  "alcohol",
+  "bewitched",
+  "biographical",
+  "blackmail",
+  "blender",
+  "blossoms",
+  "blowup",
+  "body",
+  "boomerang",
+  "boombox",
+  "bowel",
+  "businesswoman",
+  "business",
+  "butterfly",
+  "caffeine",
+  "calculator",
+  "camping",
+  "candlelight",
+  "car",
+  "carmine",
+  "ceiling",
+  "chad",
+  "champions",
+  "checkbook",
+  "checking",
+  "chocolate",
+  "cleanup",
+  "clinic",
+  "clipboard",
+  "clown",
+  "coconut",
+  "color",
+  "combing",
+  "command",
+  "communicated",
+  "conclusion",
+  "conduct",
+  "converge",
+  "corny",
+  "cosmetics",
+  "craft",
+  "craving",
+  "criminal",
+  "crimes",
+  "crucifix",
+  "crucifixion",
+  "crucify",
+  "crud",
+  "cruddy",
+  "crude",
+  "crudely",
+  "cruel",
+  "cruelly",
+  "cruelty",
+  "cruise",
+  "cruised",
+  "cruiser",
+  "cruisers",
+  "cruises",
+  "cruising",
+  "crumb",
+  "crumble",
+  "crumbled",
+  "crumbles",
+  "crumbling",
+  "crumbs",
+  "crummy",
+  "disarm",
+  "disarmed",
+  "disarming",
+  "disarray",
+  "disassembled",
+  "disaster",
+  "disasters",
+  "disastrous",
+  "disbarred",
+  "disbelief",
+  "disc",
+  "discarded",
+  "discerning",
+  "discharge",
+  "discharged",
+  "disciples",
+  "doing",
+  "dojo",
+  "dol",
+  "dolce",
+  "dole",
+  "doling",
+  "doll",
+  "dollar",
+  "dollars",
+  "dolled",
+  "dollhouse",
+  "dollop",
+  "dolls",
+  "dolly",
+  "dolphin",
+  "dolphins",
+  "dolt",
+  "dom",
+  "domain",
+  "dome",
+  "domes",
+  "domestic",
+  "drew",
+  "dribble",
+  "dribbles",
+  "dribbling",
+  "dried",
+  "drier",
+  "dries",
+  "drift",
+  "drifted",
+  "drifter",
+  "drifting",
+  "driftwood",
+  "drill",
+  "drilled",
+  "drilling",
+  "drink",
+  "drinker",
+  "drinkers",
+  "drinking",
+  "drinks",
+  "drip",
+  "dripped",
+  "exploded",
+  "explodes",
+  "exploding",
+  "exploit",
+  "exploitation",
+  "exploited",
+  "exploiting",
+  "exploration",
+  "exploratory",
+  "explore",
+  "explored",
+  "explorer",
+  "explorers",
+  "exploring",
+  "explosion",
+  "explosions",
+  "explosive",
+  "extreme",
+  "extremely",
+  "extremes",
+  "extremism",
+  "extremists",
+  "extremities",
+  "extricate",
+  "eye",
+  "eyeball",
+  "eyeballs",
+  "eyebrow",
+  "eyebrows",
+  "eyed",
+  "eyeful",
+  "eyeing",
+  "eyelash",
+  "eyelashes",
+  "eyelids",
+  "eyeliner",
+  "eyes",
+  "eyesight",
+  "eyesore",
+  "eyewitness",
+  "eyewitnesses",
+  "factory",
+  "facts",
+  "factual",
+  "faculties",
+  "faculty",
+  "fad",
+  "fade",
+  "faded",
+  "fades",
+  "fading",
+  "fads",
+  "fag",
+  "faggot",
+  "faggots",
+  "faggy",
+  "fags",
+  "fail",
+  "failed",
+  "failing",
+  "failings",
+  "fails",
+  "failure",
+  "failures",
+  "faint",
+  "fainted",
+  "fainter",
+  "faintest",
+  "fainting",
+  "fair",
+  "fairer",
+  "fairest",
+  "fairgrounds",
+  "fairies",
+  "gangsters",
+  "gangway",
+  "ganja",
+  "gap",
+  "gaps",
+  "gar",
+  "garage",
+  "garages",
+  "garb",
+  "garbage",
+  "garcon",
+  "garden",
+  "gardener",
+  "gardeners",
+  "gardenias",
+  "gardening",
+  "gardens",
+  "gargantuan",
+  "gargling",
+  "gargoyle",
+  "gear",
+  "geared",
+  "gearing",
+  "gears",
+  "gearshift",
+  "gecko",
+  "ged",
+  "gee",
+  "geek",
+  "geeks",
+  "geeky",
+  "gees",
+  "generals",
+  "generate",
+  "generated",
+  "generates",
+  "generating",
+  "generation",
+  "generations",
+  "generator",
+  "generators",
+  "generic",
+  "generosity",
+  "generous",
+  "generously",
+  "genes",
+  "genesis",
+  "genetic",
+  "grave",
+  "gravel",
+  "gravely",
+  "graves",
+  "gravest",
+  "graveyard",
+  "graveyards",
+  "gravity",
+  "gravy",
+  "gray",
+  "grazed",
+  "grazing",
+  "grease",
+  "greaseball",
+  "greased",
+  "greasing",
+  "greasy",
+  "great",
+  "guard",
+  "guarded",
+  "guardian",
+  "guardians",
+  "guardianship",
+  "guarding",
+  "guardrail",
+  "guards",
+  "guava",
+  "guerilla",
+  "guerillas",
+  "guerrilla",
+  "guerrillas",
+  "guess",
+  "guessed",
+  "guesses",
+  "guessing",
+  "guest",
+  "guesthouse",
+  "guests",
+  "guff",
+  "guidance",
+  "guide",
+  "guided",
+  "guidelines",
+  "guides",
+  "guiding",
+  "guild",
+  "haircut",
+  "haircuts",
+  "hairdo",
+  "hairdresser",
+  "hairdressers",
+  "haired",
+  "hairless",
+  "hairline",
+  "hairnet",
+  "hairpin",
+  "hairs",
+  "hairstyles",
+  "hairy",
+  "hale",
+  "half",
+  "halfback",
+  "halfway",
+  "halibut",
+  "halitosis",
+  "hall",
+  "hallelujah",
+  "hallmark",
+  "hallo",
+];
+
+let cities = [
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Afganistan",
+  "Angola",
+  "Austria",
+  "Azerbaijan",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Benin",
+  "Bolivia",
+  "Botswana",
+  "Brazil",
+  "Bulgaria",
+  "Cameroon",
+  "Chad",
+  "China",
+  "Chile",
+  "Colombia",
+  "Cuba",
+  "Dominica",
+  "Egypt",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Finland",
+  "France",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Hong Kong",
+  "Hungary",
+  "Honduras",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Japan",
+  "Jordan",
+  "Latvia",
+  "Lebanon",
+  "Libya",
+  "Mali",
+  "Malaysia",
+  "Malta",
+  "Mexico",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Morocco",
+  "Nepal",
+  "Netherlands",
+];
+
+let hard = [
+  "abandonment",
+  "abnormalities",
+  "abomination",
+  "absolutely",
+  "accommodations",
+  "accomplishments",
+  "accountability",
+  "acknowledgement",
+  "acquaintances",
+  "actualization",
+  "administrators",
+  "advertisement",
+  "affectionately",
+  "aforementioned",
+  "aggressively",
+  "agricultural",
+  "alliteration",
+  "alphabetically",
+  "alphabetized",
+  "alternatives",
+  "ambivalence",
+  "anesthesiologist",
+  "anesthesiology",
+  "announcements",
+  "antagonizing",
+  "anthropologists",
+  "antidepressants",
+  "antihistamines",
+  "aphrodisiac",
+  "apologetic",
+  "appendicitis",
+  "bachelorette",
+  "backpacking",
+  "backstabbing",
+  "ballplayers",
+  "beheading",
+  "beleaguered",
+  "believable",
+  "bellyaching",
+  "bellybutton",
+  "belvedere",
+  "benefactors",
+  "benevolence",
+  "billionaires",
+  "biologically",
+];
+
+const startText = document.querySelector(".start-text");
+const startBtn = document.querySelector(".start-btn");
+const menuWindow = document.querySelector(".menu");
+const wordBox = document.querySelector(".word-box");
+const timeBox = document.querySelector(".time");
+const scoreBox = document.querySelector(".score-box");
+const errorBox = document.querySelector(".errors-box");
+const levelsBtn = document.querySelectorAll(".levels-btn");
+const levelsWindow = document.querySelector(".levels");
+const returnBtn = document.querySelector(".return-btn");
+const quitBtn = document.querySelector(".quit-btn");
+const quitBox = document.querySelector(".quit-box");
+const englishAllBtn = document.querySelector(".english-all");
+const englishCountiesBtn = document.querySelector(".english-countries");
+const englishHardBtn = document.querySelector(".english-hard");
+const levelSwitchMsg = document.querySelector(".switch-level-msg");
+const healthBar = document.querySelector(".health-bar");
+// Выбранный список слов (по умолчанию arrayword)
+let selectedArray = arrayword;
+let play = false;
+let startTyping = new Date();
+let endTyping = new Date();
+startBtn.addEventListener("click", () => {
+  console.log("Game started");
+  menuWindow.style.display = "none";
+  setTimeout(() => {
+    wordBox.innerHTML = 1;
+  }, 1000);
+  setTimeout(() => {
+    wordBox.innerHTML = "2";
+  }, 2000);
+  setTimeout(() => {
+    wordBox.innerHTML = "3";
+  }, 3000);
+  setTimeout(() => {
+    displayWord();
+    play = true;
+  }, 4000);
+});
+
+levelsBtn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    menuWindow.style.display = "none";
+    levelsWindow.style.display = "flex";
+  });
+});
+
+returnBtn.addEventListener("click", () => {
+  menuWindow.style.display = "flex";
+  levelsWindow.style.display = "none";
+});
+
+englishAllBtn.addEventListener("click", () => {
+  selectedArray = arrayword;
+  levelSwitchMsg.style.opacity = 100 + "%";
+  levelSwitchMsg.innerHTML = `Был выбран список всех слов на английском`;
+  setTimeout(() => {
+    levelSwitchMsg.style.opacity = 0;
+  }, 2000);
+});
+englishCountiesBtn.addEventListener("click", () => {
+  selectedArray = cities;
+  levelSwitchMsg.style.opacity = 100 + "%";
+  levelSwitchMsg.innerHTML = `Был выбран список стран на английском`;
+  setTimeout(() => {
+    levelSwitchMsg.style.opacity = 0;
+  }, 2000);
+});
+englishHardBtn.addEventListener("click", () => {
+  selectedArray = hard;
+  levelSwitchMsg.style.opacity = 100 + "%";
+  levelSwitchMsg.innerHTML = `Был выбран список сложных слов на английском`;
+  setTimeout(() => {
+    levelSwitchMsg.style.opacity = 0;
+  }, 2000);
+});
+
+// Обработка убегания кнопки выхода
+quitBtn.addEventListener("mouseenter", (event) => {
+  var pos = quitBtn.getBoundingClientRect();
+  var elem_left = pos.left.toFixed(0);
+  var elem_top = pos.top.toFixed(0);
+  var x = event.pageX - elem_left;
+  var y = event.pageY - elem_top;
+  quitBtn.style.top = y + 100 + "px";
+  quitBtn.style.left = x + 200 + "px";
+  quitBtn.innerHTML = ":(";
+});
+
+quitBox.addEventListener("mouseleave", (event) => {
+  setTimeout(() => {
+    quitBtn.style.left = "290px";
+    quitBtn.style.top = "0px";
+    quitBtn.innerHTML = "Quit)";
+  }, 5000);
+});
+
+let selectedWord = "";
+let score = 0;
+let allScore = 0;
+let errors = 0;
+
+let errorVolume = 0.2;
+let clickVolume = 0.2;
+let damageVolume = 0.2;
+let winVolume = 0;
+let musicVolume = 0;
+
+var audioError = new Audio("./sounds/error3.wav");
+audioError.volume = errorVolume;
+
+var audioClick = new Audio("./sounds/click.wav");
+audioClick.volume = clickVolume;
+
+var audioDamage = new Audio("./sounds/mixkit-impact-of-a-blow-2150.wav");
+audioDamage.volume = damageVolume;
+let bosshp = 100;
+let difficultLevel = 100; // от 100(самый сложный) до 5(легкий)
+// Функция рандома и вывода слов
+function displayWord() {
+  selectedWord =
+    selectedArray[Math.floor(Math.random() * selectedArray.length)];
+  wordBox.innerText = selectedWord;
+  startTyping = new Date();
+  console.log(bosshp);
+  if (allScore >= 1000) {
+    bosshp -= score / difficultLevel;
+    healthBar.style.background = `linear-gradient(90deg, rgba(255,0,0,1) ${bosshp}%, rgba(255,0,0,1) ${bosshp}%, rgba(21,0,0,1) ${bosshp}%, rgba(0,0,0,1) 96%)`;
+    audioDamage.play();
+  }
+  if (errors > 5) {
+    bosshp += 20;
+    hp.innerHTML = bosshp;
+  }
+  hp.innerHTML = Math.round(bosshp);
+}
+
+// console.log((date2.getTime() - date.getTime()) / 1000);
+let numEnterLetter = 0;
+document.addEventListener("keydown", (event) => {
+  if (play) {
+    const letter = event.key.toLowerCase();
+    selectedWord = selectedWord.toLocaleLowerCase();
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+      if (selectedWord[numEnterLetter] == letter) {
+        wordInBox = selectedWord
+          .split("")
+          .slice(numEnterLetter + 1, selectedWord.length)
+          .join("");
+
+        wordBox.innerHTML = wordInBox;
+
+        audioClick.play();
+
+        numEnterLetter++;
+        if (numEnterLetter >= selectedWord.length) {
+          numEnterLetter = 0;
+          endTyping = new Date();
+          time = ((endTyping.getTime() - startTyping.getTime()) / 1000).toFixed(
+            2
+          );
+
+          // Счет очков (
+          // За каждую букву +100 очков,
+          // если слово введено меньше чем за 1 секунду *1.5,
+          // если слово больше 6 букв и введено меньше чем за 1 секунду *2,
+          // если слово введено больше чем за 4 секунды, то очки не прибавляются
+          // если слово введено с более чем 3 ошибками, то очки не прибавляются
+          // за каждую ошибку -50 очков)
+          score = selectedWord.length * 100;
+          if (selectedWord.length >= 6 && time < 2) {
+            score *= 2;
+          } else if (time < 1) {
+            score *= 1.5;
+          } else if (time > 4) {
+            score *= 0;
+          }
+
+          if (errors >= 5) {
+            score *= 0;
+          } else if (errors < 5) {
+            score -= 50 * errors;
+          }
+
+          allScore += score;
+          errors = 0;
+          scoreBox.innerHTML = `${allScore}: + ${score}`;
+          errorBox.innerHTML = errors;
+          timeBox.innerHTML = time;
+          displayWord();
+        }
+      } else {
+        // Считаем неправильно введеные буквы в одном слове
+        errors++;
+        // Звук ошибки
+        audioError.play();
+        errorBox.innerHTML = errors;
+      }
+    }
+  }
+});
+
+// Сделать кнопки паузы и выхода
